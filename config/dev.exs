4874f4220bb213e53333a0d83d32e42194c7f2ad
ICBluetab <inigo.crespo@bluetab.net>
2018-04-12 12:54:00 +0200

    Fix config for swagger

--- config/dev.exs
+++ config/dev.exs
@@ -8,6 +8,7 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :td_auth, TdAuthWeb.Endpoint,
   http: [port: 4001],
+  url: [host: "localhost", port: 4001],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
