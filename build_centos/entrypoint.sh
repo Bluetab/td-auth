b6b3071076a95d0bad64d6f12e2ea90d07ae238d
Nacho <ignacio.hidalgo@bluetab.net>
2018-05-09 10:25:48 +0200

    modify entrypoint

--- build_centos/entrypoint.sh
+++ build_centos/entrypoint.sh
@@ -4,5 +4,15 @@ cp -R /code /working_code
 chmod -R 777 /working_code
 cd /working_code
 
+echo "Starting deploy"
+
+MIX_ENV=prod
+mix local.rebar --force
+rm -rf ./_build
+mix deps.clean --all
+mix deps.get
+
 mix release --env=prod
-yes | cp _build/dev/rel/td_auth/releases/0.0.1/td_auth.tar.gz /code/dist/
+cp _build/dev/rel/td_auth/releases/0.0.1/td_auth.tar.gz /code/dist/
+
+echo "Finished deployment"
