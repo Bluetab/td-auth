f30d4c44668322c510282afe6f07361221cb9494
joseluis.fernandez@bluetab.net <joseluis.fernandez@bluetab.net>
2018-06-21 16:00:56 +0200

    Improving dockerfile adding multistage building

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TdAuth.Mixfile do
   def project do
     [
       app: :td_auth,
-      version: "0.0.1",
+      version: "1.1.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
