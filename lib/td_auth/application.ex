2eaca0321e941979473740dca142aec523ab3dbe
Tom Crossland <tom.crossland@bluetab.net>
2018-11-08 11:48:07 +0100

    fix: prevent failure in application startup if OIDC variables are absent

--- lib/td_auth/application.ex
+++ lib/td_auth/application.ex
@@ -13,28 +13,29 @@ defmodule TdAuth.Application do
       id: TdAuth.AclRemover,
       start: {TdAuth.AclRemover, :start_link, []}
     }
+
     # Setup metrics exporter
     PrometheusExporter.setup()
 
     # Define workers and child supervisors to be supervised
-    children = [
-      # Start the Ecto repository
-      supervisor(TdAuth.Repo, []),
-      # Start the endpoint when the application starts
-      supervisor(TdAuthWeb.Endpoint, []),
-      # Start your own worker by calling: TdAuth.Worker.start_link(arg1, arg2, arg3)
-      # worker(TdAuth.Worker, [arg1, arg2, arg3]),
-      worker(TdAuth.UserLoader, [TdAuth.UserLoader]),
-      worker(TdAuth.AclLoader, [TdAuth.AclLoader]),
-      worker(OpenIDConnect.Worker, [Application.get_env(:td_auth, :openid_connect_providers)]),
-      %{
-        id: TdAuth.CustomSupervisor,
-        start:
-          {TdAuth.CustomSupervisor, :start_link,
-           [%{children: [acl_remover_worker], strategy: :one_for_one}]},
-        type: :supervisor
-      }
-    ]
+    children =
+      [
+        # Start the Ecto repository
+        supervisor(TdAuth.Repo, []),
+        # Start the endpoint when the application starts
+        supervisor(TdAuthWeb.Endpoint, []),
+        # Start your own worker by calling: TdAuth.Worker.start_link(arg1, arg2, arg3)
+        # worker(TdAuth.Worker, [arg1, arg2, arg3]),
+        worker(TdAuth.UserLoader, [TdAuth.UserLoader]),
+        worker(TdAuth.AclLoader, [TdAuth.AclLoader]),
+        %{
+          id: TdAuth.CustomSupervisor,
+          start:
+            {TdAuth.CustomSupervisor, :start_link,
+             [%{children: [acl_remover_worker], strategy: :one_for_one}]},
+          type: :supervisor
+        }
+      ] ++ oidc_workers()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
@@ -47,5 +48,21 @@ defmodule TdAuth.Application do
   def config_change(changed, _new, removed) do
     Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp oidc_workers do
+    import Supervisor.Spec
+
+    config = Application.get_env(:td_auth, :openid_connect_providers)
+
+    if empty_config?(config), do: [], else: [worker(OpenIDConnect.Worker, [config])]
+  end
+
+  defp empty_config?(config) do
+    config
+    |> Keyword.values()
+    |> Enum.map(& &1[:client_id])
+    |> Enum.filter(&(&1 !== ""))
+    |> Enum.empty?()
   end
 end
