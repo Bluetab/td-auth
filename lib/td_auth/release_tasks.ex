ff91b6d97ae0618a7a7b74caf39bdcd1838c896e
Tom Crossland <tom.crossland@bluetab.net>
2019-01-07 02:15:23 +0100

    fix: update release tasks for distillery 2.0

--- lib/td_auth/release_tasks.ex
+++ lib/td_auth/release_tasks.ex
@@ -1,5 +1,8 @@
 defmodule TdAuth.ReleaseTasks do
-  @moduledoc false
+  @moduledoc """
+  Release tasks to run Ecto migrations and create seed data
+  """
+
   alias Ecto.Migrator
 
   @start_apps [
@@ -9,67 +12,78 @@ defmodule TdAuth.ReleaseTasks do
     :ecto
   ]
 
-  def myapp, do: :td_auth
+  @app :td_auth
+  @repos Application.get_env(@app, :ecto_repos, [])
 
-  def repos, do: Application.get_env(myapp(), :ecto_repos, [])
-
-  def seed do
-    # Run migrations
-    migrate()
-
-    # Run seed script
-    Enum.each(repos(), &run_seeds_for/1)
-
-    # Signal shutdown
-    IO.puts "Success!"
-    :init.stop()
+  def migrate do
+    start_services()
+    run_migrations()
+    stop_services()
   end
 
-  defp prepare do
-    me = myapp()
+  def seed do
+    start_services()
+    run_migrations()
+    run_seeds()
+    stop_services()
+  end
 
-    IO.puts "Loading #{me}.."
+  defp start_services do
+    IO.puts("Loading #{@app}..")
+
     # Load the code for myapp, but don't start it
-    :ok = Application.load(me)
+    Application.load(@app)
 
-    IO.puts "Starting dependencies.."
+    IO.puts("Starting dependencies..")
     # Start apps necessary for executing migrations
     Enum.each(@start_apps, &Application.ensure_all_started/1)
 
-    # Start the Repo(s) for myapp
-    IO.puts "Starting repos.."
-    Enum.each(repos(), &(&1.start_link(pool_size: 1)))
+    # Start the Repo(s) for app
+    IO.puts("Starting repos..")
+    Enum.each(@repos, & &1.start_link(pool_size: 1))
   end
 
-  def migrate do
-    prepare()
-    Enum.each(repos(), &run_migrations_for/1)
+  defp stop_services do
+    IO.puts("Success!")
+    :init.stop()
   end
 
-  def priv_dir(app), do: "#{:code.priv_dir(app)}"
+  defp run_migrations do
+    Enum.each(@repos, &run_migrations_for/1)
+  end
 
   defp run_migrations_for(repo) do
     app = Keyword.get(repo.config, :otp_app)
-    IO.puts "Running migrations for #{app}"
-    Migrator.run(repo, migrations_path(repo), :up, all: true)
+    IO.puts("Running migrations for #{app}")
+    migrations_path = priv_path_for(repo, "migrations")
+    Migrator.run(repo, migrations_path, :up, all: true)
   end
 
-  def run_seeds_for(repo) do
+  defp run_seeds do
+    Enum.each(@repos, &run_seeds_for/1)
+  end
+
+  defp run_seeds_for(repo) do
     # Run the seed script if it exists
-    seed_script = seeds_path(repo)
+    seed_script = priv_path_for(repo, "seeds.exs")
+
     if File.exists?(seed_script) do
-      IO.puts "Running seed script.."
+      IO.puts("Running seed script..")
       Code.eval_file(seed_script)
     end
   end
 
-  def migrations_path(repo), do: priv_path_for(repo, "migrations")
-
-  def seeds_path(repo), do: priv_path_for(repo, "seeds.exs")
-
-  def priv_path_for(repo, filename) do
+  defp priv_path_for(repo, filename) do
     app = Keyword.get(repo.config, :otp_app)
-    repo_underscore = repo |> Module.split |> List.last |> Macro.underscore
-    Path.join([priv_dir(app), repo_underscore, filename])
+
+    repo_underscore =
+      repo
+      |> Module.split()
+      |> List.last()
+      |> Macro.underscore()
+
+    priv_dir = "#{:code.priv_dir(app)}"
+
+    Path.join([priv_dir, repo_underscore, filename])
   end
 end
