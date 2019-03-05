af3a6a6ad2dee061848f1d0d63c1888a93fa0911
Tom Crossland <tom.crossland@bluetab.net>
2019-03-05 18:08:52 +0100

    fix: version in mix.exs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdAuth.Mixfile do
       app: :td_auth,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "2.12.5-local"
+          nil -> "2.14.0-local"
           v -> v
         end,
       elixir: "~> 1.6",
