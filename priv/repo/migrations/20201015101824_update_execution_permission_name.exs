ac99bafa3fb62c5d6a5864253fd36678a6781baa
Saul Isern <saul.isern@bluetab.net>
2020-10-15 12:32:18 +0200

    feat: [TD-3047] migration to change permission name

---
+++ priv/repo/migrations/20201015101824_update_execution_permission_name.exs
@@ -0,0 +1,21 @@
+defmodule TdAuth.Repo.Migrations.UpdateExecutionPermissionName do
+  use Ecto.Migration
+
+  alias TdAuth.Repo
+
+  import Ecto.Query
+
+  def up do
+    from(p in "permissions")
+    |> where([p], p.name == "execute_quality_rule")
+    |> update([_], set: [name: "execute_quality_rule_implementations", updated_at: ^DateTime.utc_now()])
+    |> Repo.update_all([])
+  end
+
+  def down do
+    from(p in "permissions")
+    |> where([p], p.name == "execute_quality_rule_implementations")
+    |> update([_], set: [name: "execute_quality_rule", updated_at: ^DateTime.utc_now()])
+    |> Repo.update_all([])
+  end
+end
