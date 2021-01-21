db90720448be21f358938122fa02d4dd0e431c05
Saul Isern <saul.isern@bluetab.net>
2021-01-21 08:52:20 +0100

    chore: [TD-3074] list groups and users if we have any permission in bg

--- lib/td_auth/canada/abilities.ex
+++ lib/td_auth/canada/abilities.ex
@@ -8,6 +8,8 @@ defmodule TdBg.Canada.Abilities do
   alias TdCache.Permissions
 
   defimpl Canada.Can, for: Claims do
+    @notification_groups ["business_glossary_view", "business_glossary_management"]
+
     # administrator is superpowerful
     def can?(%Claims{is_admin: true}, _action, _resource), do: true
 
@@ -27,12 +29,14 @@ defmodule TdBg.Canada.Abilities do
       authorized?(claims, :delete_acl_entry, domain_id)
     end
 
-    def can?(%Claims{jti: jti}, :list, Group) do
-      Permissions.has_any_permission_on_resource_type?(jti, [:create_acl_entry], "domain")
+    def can?(%Claims{jti: jti, groups: groups}, :list, Group) do
+      Permissions.has_any_permission_on_resource_type?(jti, [:create_acl_entry], "domain") or
+        Enum.any?(@notification_groups, &(&1 in groups))
     end
 
-    def can?(%Claims{jti: jti}, :list, User) do
-      Permissions.has_any_permission_on_resource_type?(jti, [:create_acl_entry], "domain")
+    def can?(%Claims{jti: jti, groups: groups}, :list, User) do
+      Permissions.has_any_permission_on_resource_type?(jti, [:create_acl_entry], "domain") or
+        Enum.any?(@notification_groups, &(&1 in groups))
     end
 
     def can?(%Claims{jti: jti}, :list, Role) do
