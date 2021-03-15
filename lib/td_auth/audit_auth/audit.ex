b7a4ed7c5e53a037a5f0815b7bbcdf8f2a84c6ca
Ubuntu <guillermo.r.navarrete@bluetab.net>
2021-03-15 15:33:20 +0000

    feat: [TD-3327] remove logs.  clicking in title, navigate to the structure/concept, remove link from card foot, In tab all include object source

--- lib/td_auth/audit_auth/audit.ex
+++ lib/td_auth/audit_auth/audit.ex
@@ -5,34 +5,20 @@ defmodule TdAuth.AuditAuth.Audit do
 
   import TdAuth.Audit.AuditSupport, only: [publish: 5]
 
-  require Logger
-
   def login_attempt(access_method, user_name) do
-    Logger.info("login_attempt access_method #{inspect(access_method)}")
-    Logger.info("login_attempt user_name #{inspect(user_name)}")
-
     payload = %{
       "user_name" => user_name,
       "access_method" => access_method
     }
-    Logger.info "login_attempt payload #{inspect payload}"
-    result = publish("login_attempt", "auth", nil, nil, payload)
-    Logger.info("login_attempt result #{inspect(result)}")
-    result
+    publish("login_attempt", "auth", nil, nil, payload)
+
   end
 
   def login_success(access_method, user) do
-    Logger.info("login_success access_method #{inspect(access_method)}")
-    Logger.info("login_success user #{inspect(user.id)}")
-
     payload = %{
       "user_name" => user.user_name,
       "access_method" => access_method
     }
-
-    Logger.info("login_success payload #{inspect(payload)}")
-    result = publish("login_success", "auth", user.id, user.id, payload)
-    Logger.info("login_success result #{inspect(result)}")
-    result
+    publish("login_success", "auth", user.id, user.id, payload)
   end
 end
