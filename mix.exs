c6ee90062d744799e2aa06e799614a62414d2865
tom.crossland@bluetab.net <tom.crossland@bluetab.net>
2022-03-14 12:03:05 +0000

    v4.40.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdAuth.Mixfile do
       app: :td_auth,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "4.38.0-local"
+          nil -> "4.40.0-local"
           v -> v
         end,
       elixir: "~> 1.11",
@@ -70,7 +70,7 @@ defmodule TdAuth.Mixfile do
       # See https://github.com/handnot2/esaml/issues/29
       {:cowboy, "~> 2.7", override: true},
       {:quantum, "~> 3.0"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.40.2"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.40.3"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:cabbage,
        git: "https://github.com/Bluetab/cabbage", branch: "feature/background", only: :test},
