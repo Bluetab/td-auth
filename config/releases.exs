7c0f024e503345b66f37b1b9b0e4d99673ce8689
euclydes.young@bluetab.net <euclydes.young@bluetab.net>
2023-04-18 11:49:58 +0000

    TD-5297 Added 'DB_SSL' environment variable for Database SSL connection

--- config/releases.exs
+++ config/releases.exs
@@ -19,7 +19,8 @@ config :td_auth, TdAuth.Repo,
   hostname: System.fetch_env!("DB_HOST"),
   port: System.get_env("DB_PORT", "5432") |> String.to_integer(),
   pool_size: System.get_env("DB_POOL_SIZE", "4") |> String.to_integer(),
-  timeout: System.get_env("DB_TIMEOUT_MILLIS", "15000") |> String.to_integer()
+  timeout: System.get_env("DB_TIMEOUT_MILLIS", "15000") |> String.to_integer(),
+  ssl: System.get_env("DB_SSL", "") |> String.downcase() == "true"
 
 config :joken,
   default_signer: [
