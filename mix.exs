ffc32960a041ceede187258ade5ad5aa03d141b0
euclydes.young@bluetab.net <euclydes.young@bluetab.net>
2025-06-30 16:51:14 +0000

    TD-7307 Merge release 7.7.0 into develop

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdAuth.Mixfile do
       app: :td_auth,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "7.6.0-local"
+          nil -> "7.7.0-local"
           v -> v
         end,
       elixir: "~> 1.18",
